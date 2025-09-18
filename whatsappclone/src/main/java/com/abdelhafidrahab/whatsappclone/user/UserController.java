4abbb6a85e36addb13350f99f0ccdee62f40bd82
Abdelhafid Rahab <abdelhafidrahab@yahoo.com>
2025-09-18 10:03:05 +0100

    feat(user): Implement endpoint to find all users except self

---
+++ whatsappclone/src/main/java/com/abdelhafidrahab/whatsappclone/user/UserController.java
@@ -0,0 +1,27 @@
+package com.abdelhafidrahab.whatsappclone.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAllUsers(Authentication authentication) {
+        return ResponseEntity.ok(userService.getAllUsersExceptSelf(authentication));
+    }
+    
+}
